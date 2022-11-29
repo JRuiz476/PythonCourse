96c5f6ab547f2edc4cc595e7638328a3027b4289
JRuiz476 <julio.ruiz@catolica.edu.sv>
2022-11-28 20:14:12 -0600

    #3 modificado, #4 añadido

--- Modulo01/003-Funcion_Print.py
+++ Modulo01/003-Funcion_Print.py
@@ -1,2 +1,14 @@
 # Hola Mundo con función "print"
 print ("Hola, mundo. ¡Estoy programando con Python!")
+
+# Printing con varias líneas
+print("""
+Hola
+¿En serio esto no va tan identado?
+Python, tu identación es una estafa
+Dos puntos uve xd
+"""
+)
+
+# Comillas inception xd
+print('A este truco le llamo "dejar la pasantía" ;v')
